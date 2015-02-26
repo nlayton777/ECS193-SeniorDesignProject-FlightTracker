e0555464105bff7a717db85c24385952a573088e
Rupali Saiya <rcsaiya@ucdavis.edu>
2015-02-25 16:42:47 -0800

    Formatting of collpase/expanding in table

--- temp.php
+++ temp.php
@@ -9,6 +9,18 @@
 	<script src="jquery-2.1.3.js"/></script>
 	<script src="bootstrap.js"></script>
 
+
+    <!-- FOR DROP DOWN IN TABLE -->
+	<link rel="stylesheet" href="../../jqwidgets/styles/jqx.base.css" type="text/css" />
+	<script type="text/javascript" src="../../scripts/jquery-1.7.2.min.js"></script>
+	<script type="text/javascript" src="../../jqwidgets/jqxcore.js"></script>
+	<script type="text/javascript" src="../../jqwidgets/jqxdata.js"></script>
+	<script type="text/javascript" src="../../jqwidgets/jqxbuttons.js"></script>
+	<script type="text/javascript" src="../../jqwidgets/jqxscrollbar.js"></script>
+	<script type="text/javascript" src="../../jqwidgets/jqxmenu.js"></script>
+	<script type="text/javascript" src="../../jqwidgets/jqxgrid.js"></script>
+	<script type="text/javascript" src="../../jqwidgets/jqxgrid.selection.js"></script>
+	<script type="text/javascript" src="../../jqwidgets/jqxdropdownbutton.js"></script> 
     </head>
 
     <body>
@@ -49,19 +61,35 @@
 
 	    <table class="table table-hover background-color:#CCF5F6">
 		<tr>
-		    <th>Carrier</th>
 		    <th>Price</th>
-		    <th colspan='4'>Flight Info</th>
+		    <th colspan='6'>Flight Info</th>
 		</tr>
 
 		<tr>
-		    <td>Southwest</td>
 		    <td>$500.00</td>
-		    <td> 10:00AM <strong>SFO</strong> &rarr; 1:00PM <strong>JFK</strong>    6h0m    (1 stop PHX)</td>
+		    <td> 10:00AM <strong>SFO</strong> &rarr; 1:00PM <strong>JFK</strong>    6h0m    (1 stop PHX)
+			    <div id="divTest">
+				<table cellspacing="10" cellpadding="10" id="expandedtable" class="table">
+				    <tr>
+					    <th>Stopover Number </th>
+					    <th>Point A</th>
+					    <th>Point B</th>
+				    </tr>
+
+				    <tr>
+					<td>1</td>
+					<td>11:00AM PHX</td>
+					<td>2:00PM JFK</td>
+    				    </tr>
+			    </table>
+			    </div>
+		    </td>
+		    <td>
+			<input type="button" id="btnExpCol" onclick="Expand();" value="Expand"/>
+		    </td>
 		</tr>
 
 		<tr>
-		    <td>US Airways</td>
 		    <td>$550.00</td>
 		    <td> 10:30AM <strong>SFO</strong> &rarr; 1:45PM <strong>JFK</strong>    6h15m    (1 stop SEA)</td>
 		</tr>	
@@ -73,6 +101,29 @@
     </div>
 
 </div>
+
+<!-- Button animation code -->
+<script>
+    window.onload=function(){
+	$('#divTest').hide();
+    
+    };
+    $(document).ready(function () {
+     $('#btnExpCol').click(function () {
+        if ($(this).val() == 'Collapse') {
+		                         
+					 
+	$('#divTest').stop().slideUp('3000');
+	    $(this).val('Expand');
+	} else {
+		    
+	$('#divTest').stop().slideDown('3000');
+	    $(this).val('Collapse');		     
+	}										                 });
+	});
+</script>
+																		            
+
 
 </body>
 

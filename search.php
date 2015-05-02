2d0694c7238be551736bfec33e3745dda00e5a49
Nick Layton <nllayton@ucdavis.edu>
2015-05-02 11:14:28 -0700

    fixing repo

--- search.php
+++ search.php
@@ -13,51 +13,51 @@
 
 	<script>
 	    //  VALIDATION
-function check()
-{
-    //check email
-    if (document.layers||document.getElementById||document.all)
-    {
+	function check()
+	{
+	    //check email
+	    if (document.layers||document.getElementById||document.all)
+	    {
 
-		var str=document.searchwindow.email.value
-    	var filter=/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
-    	if (filter.test(str))
-			var testresults=true
-    	else{
-			alert("Please input a valid email address!")
-			return false;
-			}
-	}
-    	
-	//validation search time is after depart date
- //    var currentSecs = new Date().getTime()/1000; //get time right now in seconds 
-	    
- //    var searchTime = document.getElementById("numHours").value;
- //    var searchSecs = searchTime * 60 * 60; //search window time in seconds 
-
- //    var CurrentSearchSecs = currentSecs + searchSecs;
-
-    
- //   //get Departure date and convert to seconds 
-	 <?php 
-	 	require_once('./flight_tracker.php');
-
-		$post = $_POST;
-	// 	echo "var departureDate = \"{$post['depart_date']}\";";
-	 ?>   
-
-
-	// var departSecs = getDateFromFormat(departureDate, "MM/DD/YYYY");
-
-	// departSecs = departSecs/1000;
-
-	// if((currentSecs + searchSecs) > departSecs)
-	// {
-	// 	alert("Please choose a search time that will complete before your departure date.")
+			var str=document.searchwindow.email.value
+		var filter=/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+		if (filter.test(str))
+				var testresults=true
+		else{
+				alert("Please input a valid email address!")
+				return false;
+				}
+		}
 		
-	// }
+		//validation search time is after depart date
+	 //    var currentSecs = new Date().getTime()/1000; //get time right now in seconds 
+		    
+	 //    var searchTime = document.getElementById("numHours").value;
+	 //    var searchSecs = searchTime * 60 * 60; //search window time in seconds 
 
-}
+	 //    var CurrentSearchSecs = currentSecs + searchSecs;
+
+	    
+	 //   //get Departure date and convert to seconds 
+		 <?php 
+			require_once('./flight_tracker.php');
+
+			$post = $_POST;
+		// 	echo "var departureDate = \"{$post['depart_date']}\";";
+		 ?>   
+
+
+		// var departSecs = getDateFromFormat(departureDate, "MM/DD/YYYY");
+
+		// departSecs = departSecs/1000;
+
+		// if((currentSecs + searchSecs) > departSecs)
+		// {
+		// 	alert("Please choose a search time that will complete before your departure date.")
+			
+		// }
+
+	}
 	//end  validation
 	</script>
 
@@ -115,6 +115,7 @@ function check()
 				    echo $post['return_date'];
 				echo "</h3>";
 			    ?>
+
 			</div>
 
 			<div class="col-md-6" id="background-info">
